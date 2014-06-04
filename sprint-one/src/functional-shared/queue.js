ea52d9ca3f51781291969b917ccf636e6f769c43
Hack Reactor Students <xianhui.feng100@gmail.com>
2014-06-04 16:37:45 -0700

    finished prototypal and functional-shared queue

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,9 +1,39 @@
 var makeQueue = function(){
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  var len = 0;
+  var storage = {};
+  var ind = 0;
+  var counter = 0;
+  _.extend(someInstance,  queueMethods);
+  someInstance.len = len;
+  someInstance.storage = storage;
+  someInstance.ind = ind;
+  someInstance.counter = counter;
+
+  return someInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  size: function(){
+    return this.len;
+  },
+  enqueue: function(value){
+    this.storage[this.ind] = value;
+    this.len++;
+    this.ind++;
+  },
+  dequeue: function(){
+    if (this.len > 0){
+      this.len--;
+      var value = this.storage[this.counter];
+      delete this.storage[this.counter];
+      this.counter++;
+      return value;
+    }
+  }
+};
 
 
 
--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,36 @@
 var makeQueue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(queueMethods);
+  var len = 0;
+  var storage = {};
+  var count = 0;
+  var ind = 0;
+  someInstance.len = len;
+  someInstance.storage = storage;
+  someInstance.count = count;
+  someInstance.ind = ind;
+  return someInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  size: function(){
+    return this.len;
+  },
 
+  enqueue: function(value){
+    this.storage[this.ind] = value;
+    this.len++;
+    this.ind++;
+  },
 
+  dequeue: function(){
+    if(this.len > 0){
+      this.len--;
+      var value = this.storage[this.count];
+      delete this.storage[this.count];
+      this.count++;
+      return value;
+    }
+  }
+};

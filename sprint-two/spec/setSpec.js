b488d8e48aebdd2d509f7d548b4d5721b8f501e7
Mike <mbarton.mail@gmail.com>
2014-06-07 16:22:14 -0700

    extra credit - hash resize

--- sprint-two/spec/setSpec.js
+++ sprint-two/spec/setSpec.js
@@ -11,17 +11,17 @@ describe('set', function() {
     expect(set.remove).to.be.a("function");
   });
 
-  it('should add values to a set', function(){
+  it('should add numbers to a set', function(){
     set.add("Susan Sarandon");
-    set.add("Danny Glover");
-    expect(set.contains('Danny Glover')).to.equal(true);
+    set.add("1");
+    expect(set.contains('1')).to.equal(true);
     expect(set.contains('Susan Sarandon')).to.equal(true);
   });
 
-  it('should remove values from a set', function(){
-    set.add("Mel Gibson");
-    set.remove("Mel Gibson");
-    expect(set.contains("Mel Gibson")).to.equal(false);
+  it('should remove numbers from a set', function(){
+    set.add("4");
+    set.remove("4");
+    expect(set.contains("4")).to.equal(false);
   });
 
 });
--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,6 +1,7 @@
 var HashTable = function(){
   this._limit = 8;
   this._storage = makeLimitedArray(this._limit);
+  this._count = 0; //add items
 };
 
 HashTable.prototype.insert = function(k, v){
@@ -8,22 +9,34 @@ HashTable.prototype.insert = function(k, v){
   if (this._storage.get(i) === undefined){
     this._storage.set(i, []);
   }
+
+
+
   this._storage.each(function(miniArr, indexOfStor, storage){
     if (indexOfStor === i){
-      var key = {};
-      key[k] = v;
+      var key = [];
+      key[0] = k;
+      key[1] = v;
       miniArr.push(key);
+      this._count++;
+            console.log(this._count)
+      if (this._count > .75 * this._limit)
+        this.resize( this._limit * 2)
     }
-  });
+  }.bind(this));
+ 
 
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
   var subArr = this._storage.get(i);
+  if (subArr === undefined){
+    return null;
+  }
   for (var x = 0; x < subArr.length; x++){
-    if (subArr[x][k]){
-      return subArr[x][k];
+    if (subArr[x][0] === k){
+      return subArr[x][1];
     }
   }
   return null;
@@ -33,13 +46,37 @@ HashTable.prototype.retrieve = function(k){
 HashTable.prototype.remove = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
   var subArr = this._storage.get(i);
+  if (subArr === undefined){
+    return null;
+  }
+
   for (var x = 0; x < subArr.length; x++){
-    if (subArr[x][k]){
+    if (subArr[x][0] === k){
       subArr.splice(x,1);
+      this._count--;
+      console.log(this._count)
+      if(this._count < .25 * this._limit){
+        this.resize( this._limit/2 );
+      }
     }
   }
 };
 
+HashTable.prototype.resize = function(newSize){
+  var oldValues = this._storage;
+
+  this._storage = makeLimitedArray(newSize);
+  this._limit = newSize;
+  this._count = 0;
+
+  oldValues.each(function(bucket){
+    if (bucket === undefined){ return; }
+    for (var i = 0; i < bucket.length; i++){
+      var tuple = bucket[i];
+      this.insert(tuple[0], tuple[1])
+    }
+  }.bind(this));
+}
 /*
  * Complexity: What is the time complexity of the above functions?
  */
